cf34d83d93da6cab49b9c04d505684da4deea091
ayoubley <ayoubm3lomat2019@gmail.com>
2026-05-04 10:36:52 +0100

    Use service_role key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://fvcgtdkohqmqykbiarme.supabase.co";
 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2Y2d0ZGtvaHFtcXlrYmlhcm1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MzUyMTgsImV4cCI6MjA5MzQxMTIxOH0.kQJnDIULOXZ9-rSUCFxrrsqCy7ijY1BLo4SQ9IqcxJU";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnc2Rsdnl4cXNvamp1bWN0ZGhhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NzY2Mjg3NCwiZXhwIjoyMDkzMjM4ODc0fQ.XOjzrkhMcX2uD0C3vwcNM0io5KHSTGUYX_xpYg4va5U";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
